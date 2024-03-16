cfc95f7fe0c91571d599dae72d31a4ac9015b716
sulincix <parduscix@yandex.ru>
2024-03-16 10:15:51 +0000

    init

---
+++ build.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+umask 022
+set -e
+if [[ ! -d build ]] ; then
+    # fetch & extract rootfs
+    mkdir -p build
+    cd build
+    uri="https://dl-cdn.alpinelinux.org/alpine/edge/releases/$(uname -m)/"
+    tarball=$(wget -O - "$uri" |grep "alpine-minirootfs" | grep "tar.gz<" | \
+        sort -V | tail -n 1 | cut -f2 -d"\"")
+    wget -O "$tarball" "$uri/$tarball"
+    mkdir -p chroot
+    cd chroot
+    tar -xvf ../*$tarball
+else
+    cd build/chroot
+fi
+# fix resolv.conf
+install /etc/resolv.conf ./etc/resolv.conf
+# add repositories
+cat > ./etc/apk/repositories <<EOF
+https://dl-cdn.alpinelinux.org/alpine/edge/main
+https://dl-cdn.alpinelinux.org/alpine/edge/community
+https://dl-cdn.alpinelinux.org/alpine/edge/testing
+EOF
+# upgrade if needed
+chroot ./ apk upgrade
+cd ../../hooks
+for file in $(ls . | sort -V) ; do
+    echo "Executing: $file"
+    install $file ../build/chroot/tmp/hook
+    chroot ../build/chroot/ ash /tmp/hook
+done
+if [[ -d ../build/isowork ]] ; then
+    rm -rf ../build/isowork
+fi
+mkdir -p ../build/isowork/live
+cd ../build/isowork
+# copy kernel
+install ../chroot/boot/vmlinuz-edge vmlinuz-edge
+install ../chroot/boot/initramfs-live initramfs-live
+# remove initrd from squashfs
+rm ../chroot/boot/initramfs-*
+# create squashfs
+mksquashfs ../chroot live/filesystem.squashfs -comp gzip -wildcards
+# create grub config
+mkdir -p boot/grub/
+cat > boot/grub/grub.cfg <<EOF
+insmod all_video
+terminal_output console
+terminal_input console
+menuentry Alpine {
+    linux /vmlinuz-edge quiet boot=live
+    initrd /initramfs-live
+}
+EOF
+# create iso
+cd ../
+grub-mkrescue isowork -o alpine.iso
